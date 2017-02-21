984c06522bc2d86c43979b8b1539485c27e874ef
= <jbcousinat@gmail.com>
2017-02-21 12:00:48 +0100

    delete params for graveyards

--- app/controllers/graveyards_controller.rb
+++ app/controllers/graveyards_controller.rb
@@ -3,12 +3,6 @@ class GraveyardsController < ApplicationController
     @graveyards = Graveyard.all
   end
 
-private
-
-  def graveyards_params
-    params.require(:graveyard).permit(:city, :nb_places, :picture_graveyard)
-  end
-
 end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,3 +9,4 @@ Rails.application.routes.draw do
   resources :bookings, only: [:index, :edit, :update, :destroy]
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
