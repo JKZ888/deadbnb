d66c0ea05d352ab73a5ce2044e083d907fad517b
Buffin Francois <fbuffin69@gmail.com>
2017-02-21 14:13:17 +0100

    first version of TombsController

--- app/controllers/tombs_controller.rb
+++ app/controllers/tombs_controller.rb
@@ -1,27 +1,39 @@
 class TombsController < ApplicationController
 
-
-  def index
-
-  end
-
   def new
-
+    @tomb = Tomb.new
   end
 
   def create
-
+    @tomb = Tomb.new(tomb_params)
+    if @tomb.save
+      redirect_to graveyards_path
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @tomb = Tomb.find(params[:id])
   end
 
   def update
-
+    if @tomb.update(tomb_params)
+      redirect_to graveyards_path
+    else
+      render :edit
+    end
   end
 
   def delete
-
+    @tomb.destroy
+    redirect_to graveyards_path
   end
+
+  private
+
+  def tomb_params
+    params.require(:tomb).permit(:price, :description, :name_tomb, :picture_tomb)
+  end
+
 end
