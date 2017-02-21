97634fc58d37296c0e65aa8547a721db3b789683
Buffin Francois <fbuffin69@gmail.com>
2017-02-21 11:13:39 +0100

    Initialize TombsController

---
+++ app/controllers/tombs_controller.rb
@@ -0,0 +1,27 @@
+class TombsController < ApplicationController
+
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+end
