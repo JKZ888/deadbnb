90d6a1fbebbc17cec82ff2425af76443843cd5d7
= <jbcousinat@gmail.com>
2017-02-21 14:13:51 +0100

    indentation corrected

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
 # devise require root such as
-root :to => 'graveyards#index'
-devise_for :users
+  root :to => 'graveyards#index'
+  devise_for :users
   resources :graveyards, only: [:index] do
     resources :tombs, only: [:index]
   end
