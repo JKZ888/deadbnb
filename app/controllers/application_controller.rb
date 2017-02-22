a1be0cf9e20cd03019ea0d98c36d6c522aaac3ad
Thomas Birien <thomasbirien@gmail.com>
2017-02-22 16:46:42 +0100

    prepare for edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :birth_date])
 
     # # For additional in app/views/devise/registrations/edit.html.erb
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :birth_date, :email])
   end
 end
